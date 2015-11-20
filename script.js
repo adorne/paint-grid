2ccb2834471ec4b5cb0a0bcbc586d4a9eaa07b8b
Adorne <doriwield@gmail.com>
2015-11-20 17:43:50 +0500

    Final work.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var nDivs = 256;
+var nDivs = 100;
 
 $(document).ready(function() {
   var containerHeight = 500; // высота контейнера
@@ -9,32 +9,44 @@ $(document).ready(function() {
   console.log(".container height is " + containerHeight);
   console.log(".container width is " + containerWidth);
 
-  var nRowDivs = Math.sqrt(nDivs); // количество строк квадратов
+  function newGrid(n) {
+    var nRowDivs = Math.sqrt(n); // количество строк квадратов
+    var innerDivWidth = containerWidth / nRowDivs; // ширина квадратов
+    var innerDivHeight = innerDivWidth; // высота квадратов
 
-  var innerDivWidth = containerWidth / nRowDivs; // ширина квадратов
-  var innerDivHeight = innerDivWidth; // высота квадратов
+    for (var rows = 1; rows <= nRowDivs; rows++) {
+      for (var i = 1; i <= nRowDivs; i++) {
+        $('.container').append('<div class="innerDiv"></div>');
+        $('.innerDiv').css("height", innerDivHeight + "px");
+        $('.innerDiv').css("width", innerDivWidth + "px");
+      } // располагаем ячейки по i штук в ряд
 
-  for (var rows = 1; rows <= nRowDivs; rows++) {
-    for (var i = 1; i <= nRowDivs; i++) {
-      $('.container').append('<div class="innerDiv"></div>');
-      $('.innerDiv').css("height", innerDivHeight + "px");
-      $('.innerDiv').css("width", innerDivWidth + "px");
-    } // располагаем ячейки по i штук в ряд
+      $('.container').append('</br>'); // добавляем перенос после каждой строки
+    };
+  }
 
-    $('.container').append('</br>'); // добавляем перенос после каждой строки
-  };
+  newGrid(nDivs);
 
-  var innerDivColor = "white"; // начальный цвет ячейки
-  var innerDivColorNew = "black"; // новый цвет ячейки при наведении
-  //$('.innerDiv').css("background-color", innerDivColor);
-
-  $('.innerDiv').hover(function() {
-      $(this).css("background-color", innerDivColorNew);
-    }
-  );
-
-  $('button').click(function() {
+  $('button[name=reset-grid]').click(function() {
     $('.container > div').css("background-color", innerDivColor);
-  });
+  }); // возвращаем изначальный цвет ячеек
+
+  $('button[name=new-grid]').click(function(){
+    var nDivsButton = $('input[name=nDivs]:checked').val();
+    $('.container > div, .container > br').remove();
+    newGrid(nDivsButton);
+    changeColor();
+  }); // создаём новую сетку
+
+    var innerDivColor = "white"; // начальный цвет ячейки
+    var innerDivColorNew = "black"; // новый цвет ячейки при наведении
+
+    function changeColor() {
+      $('.innerDiv').hover(function() {
+        $(this).css("background-color", innerDivColorNew);
+      }
+    )}; // меняем цвет ячейки при наведении
+
+    changeColor();
 
 })
